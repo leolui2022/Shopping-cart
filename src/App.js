cf4a525ffdeb24abd8e07596a8e73edc4c6388b5
Leo <luileo08@yahoo.com.hk>
2022-10-10 17:11:40 +0800

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 	const [cartItems, setCartItems] = useState([])
 
   return (
-    <BrowserRouter>
+    <BrowserRouter   basename="/Shopping-cart">
 
 		<CartContext.Provider value={{cartItems,setCartItems}}>
 
