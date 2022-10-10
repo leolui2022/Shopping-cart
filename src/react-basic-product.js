8b2386bddd15f631d65efd5b2fd8b8c6d8430009
Leo <luileo08@yahoo.com.hk>
2022-10-10 16:54:06 +0800

    first commit

---
+++ src/react-basic-product.js
@@ -0,0 +1,13 @@
+const item = [
+    {"id" : 1,"name" : "1蘋果", "price" : 5, "image" : "apple.jpg","description":"新鮮蘋果50克，一日一蘋果，醫生遠離我"},
+    {"id" : 2,"name" : "1橙", "price" : 3, "image" : "orange.jpg","description":"新鮮橙50克，又甜又好吃"},
+    {"id" : 3,"name" : "1芒果", "price" : 4, "image" : "mango.jpg","description":"新鮮芒500克，宜做甜品"},
+    {"id" : 4,"name" : "1西瓜", "price" : 20,"image" : "watermelon.jpg","description":"新鮮西瓜2公斤，夏季必備"},
+    {"id" : 5,"name" : "1藍梅", "price" : 10,"image" : "blueberry.jpg","description":"新鮮藍梅50克，補眼之寶"},
+    {"id" : 6,"name" : "1白蘿蔔", "price" : 5,"image" : "white-carrot.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
+    {"id" : 7,"name" : "1白蘿蔔", "price" : 5,"image" : "TBF00050.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
+    {"id" : 8,"name" : "1白蘿蔔", "price" : 5,"image" : "TBF00095_thumb.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
+    {"id" : 9,"name" : "1白蘿蔔", "price" : 5,"image" : "TBF00124_thumb.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
+]
+
+export default item
