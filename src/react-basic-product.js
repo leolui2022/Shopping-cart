52f4c16c39ff9b74267c9c16dc5c498d21f95814
leolui2022 <111100981+leolui2022@users.noreply.github.com>
2022-10-11 15:02:13 +0800

    Update react-basic-product.js

--- src/react-basic-product.js
+++ src/react-basic-product.js
@@ -1,13 +1,13 @@
 const item = [
-    {"id" : 1,"name" : "1蘋果", "price" : 5, "image" : "apple.jpg","description":"新鮮蘋果50克，一日一蘋果，醫生遠離我"},
-    {"id" : 2,"name" : "1橙", "price" : 3, "image" : "orange.jpg","description":"新鮮橙50克，又甜又好吃"},
-    {"id" : 3,"name" : "1芒果", "price" : 4, "image" : "mango.jpg","description":"新鮮芒500克，宜做甜品"},
-    {"id" : 4,"name" : "1西瓜", "price" : 20,"image" : "watermelon.jpg","description":"新鮮西瓜2公斤，夏季必備"},
-    {"id" : 5,"name" : "1藍梅", "price" : 10,"image" : "blueberry.jpg","description":"新鮮藍梅50克，補眼之寶"},
-    {"id" : 6,"name" : "1白蘿蔔", "price" : 5,"image" : "white-carrot.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
-    {"id" : 7,"name" : "1白蘿蔔", "price" : 5,"image" : "TBF00050.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
-    {"id" : 8,"name" : "1白蘿蔔", "price" : 5,"image" : "TBF00095_thumb.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
-    {"id" : 9,"name" : "1白蘿蔔", "price" : 5,"image" : "TBF00124_thumb.jpg","description":"新鮮白蘿蔔1公斤，宜煲湯"},
-]
+    {"id" : 1,"name" : "少女花1", "price" : 5, "image" : "littegirl1.jpg","description":"少女青春可人，人見人1"},
+    {"id" : 2,"name" : "少女花2", "price" : 3, "image" : "littegirl2.jpg","description":"少女青春可人，人見人愛2"},
+    {"id" : 3,"name" : "少女花3", "price" : 4, "image" : "littegirl3,jpg","description":"少女青春可人，人見人愛3"},
+    {"id" : 4,"name" : "少女花4", "price" : 20,"image" : "littegirl4.jpg","description":"少女青春可人，人見人愛4"},
+    {"id" : 5,"name" : "少女花5", "price" : 10,"image" : "littegirl5.jpg","description":"少女青春可人，人見人愛5"},
+    {"id" : 6,"name" : "少女花6", "price" : 5,"image" : "littegirl6.jpg","description":"少女青春可人，人見人愛6"},
+    {"id" : 7,"name" : "少女花7", "price" : 15,"image" : "TBF00050.jpg","description":"少女青春可人，人見人愛7"},
+    {"id" : 8,"name" : "少女花8", "price" : 25,"image" : "TBF00095_thumb.jpg","description":"少女青春可人，人見人愛8"},
+    {"id" : 9,"name" : "少女花9", "price" : 35,"image" : "TBF00124_thumb.jpg","description":"少女青春可人，人見人愛9"},
+]8
 
 export default item
